f17148d1f525e637def7ba3d357aa2f5090a8a91
Kevin Blaesing <kevin.blaesing@gmail.com>
2023-11-03 12:27:57 -0400

    wip

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "root";
-  home.homeDirectory = "/root";
+  imports = [ ./username.nix ];
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
