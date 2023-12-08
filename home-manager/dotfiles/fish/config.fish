c2d0cc6069308859fce85884bb11139795bb97e6
Kevin Blaesing <kevin.blaesing@gmail.com>
2023-12-08 05:36:20 +0000

    add in fzy based branch changer

--- home-manager/dotfiles/fish/config.fish
+++ home-manager/dotfiles/fish/config.fish
@@ -37,6 +37,12 @@ if test -z (pgrep ssh-agent)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
 
+function git-fuzzy-switch
+  git switch (git for-each-ref --format='%(refname:short)' refs/heads | fzy)
+end
+
+alias gfs "git-fuzzy-switch"
+
 zoxide init fish | source
 
 if type -q starship
