2b168ff4724f7595db5456b677a838d885cdfc16
Kevin Blaesing <kevin.blaesing@gmail.com>
2024-01-03 22:14:19 -0500

    helix lsp settings for python

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,6 +33,7 @@
 
     ".config/fish/config.fish".source = dotfiles/fish/config.fish;
     ".config/helix/config.toml".source = dotfiles/helix/config.toml;
+    ".config/helix/languages.toml".source = dotfiles/helix/languages.toml;
     ".config/qtile/config.py".source = dotfiles/qtile/config.py;
     ".config/zellij/config.kdl".source = dotfiles/zellij/config.kdl;
     "/.config/hypr/hyprland.conf".source = dotfiles/hyprland/hyprland.conf;
