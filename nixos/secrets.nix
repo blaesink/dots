a08c9b0fba094be12857f05a1b9e61392fbeff10
Kevin Blaesing <kevin.blaesing@gmail.com>
2024-08-20 16:06:09 -0400

    add in additional ssh key

--- nixos/secrets.nix
+++ nixos/secrets.nix
@@ -1,5 +1,6 @@
 let
   me = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDqd3hnGqK9vb/GPW4kOLr1glLw83wIO5M0nGQlvSqVU";
+  me2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJv5IcxMlG6lbMdrbypSMQ6lvK/60icQ4TS3ivtuaFUJ";
 in {
   "cloudflare_tunnel_token.age".publicKeys = [ me ];
   "cloudflare_tunnel_id.age".publicKeys = [ me ];
