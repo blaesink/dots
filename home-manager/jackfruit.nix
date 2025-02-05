fac775069a8d4f918279cad9ca1b042365d2e145
Kevin Blaesing <kevin.blaesing@aidn.ai>
2025-02-05 12:13:33 -0500

    starship prompt changes

--- home-manager/jackfruit.nix
+++ home-manager/jackfruit.nix
@@ -4,4 +4,5 @@
   home.username = "kevin";
   home.homeDirectory = "/home/kevin/";
   home.file.".config/fish/television.fish".source = dotfiles/fish/television.fish;
+  home.file.".config/starship.toml".source = dotfiles/starship/config.toml;
 }
